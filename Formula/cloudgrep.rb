cde4e1e7dc937aef09d800cafd7c07668490e6ba
Prajwol P <57973356+jarp0l@users.noreply.github.com>
2023-11-14 16:13:08 +0000

    Bump cloudgrep -> v1.0.3

--- Formula/cloudgrep.rb
+++ Formula/cloudgrep.rb
@@ -1,12 +1,12 @@
 class Cloudgrep < Formula
   desc "cloudgrep is grep for cloud storage"
   homepage "https://github.com/cado-security/cloudgrep"
-  version "1.0.2"
+  version "1.0.3"
   license "Apache-2.0"
 
   on_macos do
-    url "https://github.com/cado-security/cloudgrep/releases/download/v102/dist-osx.zip"    
-    sha256 "026405fca77af4aa38d3a17f5938046b0ee440863e297d6c70b6b5e55835f8a9"
+    url "https://github.com/cado-security/cloudgrep/releases/download/v103/dist-osx.zip"    
+    sha256 "572288ff4c5a694463a18e58c4982c7b279305c3a475bba3f57be6d3b27efbcb"
 
     def install
       bin.install "cloudgrep"
@@ -14,8 +14,8 @@ class Cloudgrep < Formula
   end
 
   on_linux do
-    url "https://github.com/cado-security/cloudgrep/releases/download/v102/dist-linux.zip"
-    sha256 "85fbd19fa81c5abe3da96e6bbc3b7d4aecb061aa48f5a8958002b1680a949683"
+    url "https://github.com/cado-security/cloudgrep/releases/download/v103/dist-linux.zip"
+    sha256 "1f12426205ed9c15453f0bf408ab00abde7a117f58d46c03de412c097c0c1ff7"
 
     def install
       bin.install "cloudgrep"
