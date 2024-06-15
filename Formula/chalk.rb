edbf740634995995c8950d65710694e2877e0762
Prajwol Pradhan <57973356+jarp0l@users.noreply.github.com>
2024-06-15 10:11:07 +0545

    Fix redundancies

--- Formula/chalk.rb
+++ Formula/chalk.rb
@@ -1,33 +1,35 @@
 class Chalk < Formula
-  desc "Chalk allows you to follow code from development, through builds and into production."
+  desc "Observability tool that provides full lifecycle visibility into your development process"
   homepage "https://crashoverride.com"
   version "0.4.3"
   license "GPL-3.0"
 
   on_macos do
-    url "https://dl.crashoverride.run/chalk/chalk-0.4.3-darwin-arm64"    
-    sha256 "11e2b87f51ff84ac131c9316670445eb4eb2f24b80382e8a82602905fe992030"
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://dl.crashoverride.run/chalk/chalk-#{version}-darwin-arm64"
+      sha256 "11e2b87f51ff84ac131c9316670445eb4eb2f24b80382e8a82602905fe992030"
 
-    def install
-      bin.install "chalk-0.4.3-darwin-arm64" => "chalk"
+      def install
+        bin.install "chalk-#{version}-darwin-arm64" => "chalk"
+      end
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://dl.crashoverride.run/chalk/chalk-0.4.3-linux-arm64"
+      url "https://dl.crashoverride.run/chalk/chalk-#{version}-linux-arm64"
       sha256 "caff4e443f082c305d92c43984714c6dd1c6a7d42f63e4b1eeffd43e0480358c"
 
       def install
-        bin.install "chalk-0.4.3-linux-arm64" => "chalk"
+        bin.install "chalk-#{version}-linux-arm64" => "chalk"
       end
     end
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://dl.crashoverride.run/chalk/chalk-0.4.3-linux-x86_64"
+      url "https://dl.crashoverride.run/chalk/chalk-#{version}-linux-x86_64"
       sha256 "a533a2ece32ecb25e5cb63b05ac180d8e8f9366dc54997f7475caa35729e742b"
 
       def install
-        bin.install "chalk-0.4.3-linux-x86_64" => "chalk"
+        bin.install "chalk-#{version}-linux-x86_64" => "chalk"
       end
     end
   end
--- Formula/cloudgrep.rb
+++ Formula/cloudgrep.rb
@@ -1,24 +1,22 @@
 class Cloudgrep < Formula
-  desc "cloudgrep is grep for cloud storage"
+  desc "Grep for cloud storage"
   homepage "https://github.com/cado-security/cloudgrep"
   version "1.0.5"
   license "Apache-2.0"
 
-  on_macos do
-    url "https://github.com/cado-security/cloudgrep/releases/download/v105/dist-osx.zip"    
-    sha256 "8eb02f2e7f48c2a77b254047fec00f869b0e315f2d08f47ebc13af18a357141c"
+  version_no_dots = version.to_s.tr(".", "")
 
-    def install
-      bin.install "cloudgrep"
-    end
+  on_macos do
+    url "https://github.com/cado-security/cloudgrep/releases/download/v#{version_no_dots}/dist-osx.zip"
+    sha256 "8eb02f2e7f48c2a77b254047fec00f869b0e315f2d08f47ebc13af18a357141c"
   end
 
   on_linux do
-    url "https://github.com/cado-security/cloudgrep/releases/download/v105/dist-linux.zip"
+    url "https://github.com/cado-security/cloudgrep/releases/download/v#{version_no_dots}/dist-linux.zip"
     sha256 "d4a4957de689c78af14d6bc25240156ecee0bfd153aee4616d82f139ac7d33e8"
+  end
 
-    def install
-      bin.install "cloudgrep"
-    end
+  def install
+    bin.install "cloudgrep"
   end
 end
