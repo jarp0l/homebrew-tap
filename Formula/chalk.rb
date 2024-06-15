86b8e34983ebf6ed1233e8e985a63ca4f5b6b83b
Prajwol Pradhan <57973356+jarp0l@users.noreply.github.com>
2024-06-15 10:37:30 +0545

    Bump chalk -> 0.4.5

--- Formula/chalk.rb
+++ Formula/chalk.rb
@@ -1,13 +1,13 @@
 class Chalk < Formula
   desc "Observability tool that provides full lifecycle visibility into your development process"
   homepage "https://crashoverride.com"
-  version "0.4.3"
+  version "0.4.5"
   license "GPL-3.0"
 
   on_macos do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://dl.crashoverride.run/chalk/chalk-#{version}-darwin-arm64"
-      sha256 "11e2b87f51ff84ac131c9316670445eb4eb2f24b80382e8a82602905fe992030"
+      sha256 "2ada0ca9041d6da6d54d04caf170b2d399f0b8d265746ab9acf82062eab9af8f"
 
       def install
         bin.install "chalk-#{version}-darwin-arm64" => "chalk"
@@ -18,7 +18,7 @@ class Chalk < Formula
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://dl.crashoverride.run/chalk/chalk-#{version}-linux-arm64"
-      sha256 "caff4e443f082c305d92c43984714c6dd1c6a7d42f63e4b1eeffd43e0480358c"
+      sha256 "33fee16ac583cb78b923a56e9c46d4e7242c310369c93f99e576955a41176067"
 
       def install
         bin.install "chalk-#{version}-linux-arm64" => "chalk"
@@ -26,7 +26,7 @@ class Chalk < Formula
     end
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
       url "https://dl.crashoverride.run/chalk/chalk-#{version}-linux-x86_64"
-      sha256 "a533a2ece32ecb25e5cb63b05ac180d8e8f9366dc54997f7475caa35729e742b"
+      sha256 "65ce259dc381d3b22d258181d0febacdab76b7778da7b0564ef52e0257182e24"
 
       def install
         bin.install "chalk-#{version}-linux-x86_64" => "chalk"
